e7e19f366ef492c9805ea1edf6b948597e96e9df
Vaibhav Sharma <vaibhav.sharma@cgt.co.in>
2019-05-28 15:46:48 +0530

    Add product page with admin control and also secure api with jsonwebtoken2

--- src/app/services/product.services.ts
+++ src/app/services/product.services.ts
@@ -47,7 +47,7 @@ export class ProductService {
 
     return this.http.post(
       // 'http://localhost:3000/api/productadd', 
-      'https://angular7-shopping-cart.herokuapp.com/api/product',
+      'https://angular7-shopping-cart.herokuapp.com/api/productadd',
     // return this.http.post('http://localhost:3000/api/login', 
       key.value, {
         headers:headers
